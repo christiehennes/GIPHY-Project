91fa6b00442f5c4e75ce4c90d0f4a4ea3371cfca
Christie Hennes <christiehennes@gmail.com>
2018-06-21 17:00:18 -0700

    Updated to show more gifs

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 
 //Variable Declaration
 
-let citiesList = ["san diego", "new york", "los angeles", "chicago"];
+let citiesList = ["san diego", "new york", "los angeles", "chicago", "paris", "milan", "tokyo", "rome"];
+let localOffset = 0; //Create an offset to add more gifs on page 
+let currentSearch = ''; //To update with current search term
 
 
-
-
-
-// PSEUDO CODE
-
-// Function: Display buttons --> Dynamically put the buttons on the page (clear out all and add new)
+// Dynamically display buttons on the page 
 function displayButtons(){
 
     //Clear out the buttons to start
     $('#buttons-list').empty();
 
     citiesList.forEach(function(item){
-        console.log(item);
-
+        //Create the button for each array item and add to the button list 
         let button = `<button data-name="${item}" class="button">${item}</button>`;
-        console.log("Data attr: " + $(button).attr("data-name"));
         $('#buttons-list').append(button);
     })
 
 };
 
-function displayGifs(search){
+//Make the API call and display gifs on the page 
+function displayGifs(search, offset){
 
-    $('#images-display').empty();
+    //Empty out the images div and replace after API call
+    if(!offset){ $('#images-display').empty(); };
+    $('#show-more').empty();
+
+    //Update the offset to display more gifs
+    let newOffset = offset * 10;
 
     //Define variables for API call
     let apiKey = 'zsklsDJa6xWAE1ExbSiP3h2pRhDwxw7p';
-    let queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=10`;
+    let queryUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=10&offset=${newOffset}`;
 
     console.log(queryUrl);
 
@@ -41,19 +42,14 @@ function displayGifs(search){
         method: "GET"
     }).then(function(response){
 
-        //console.log("Inside response");
-        //console.log(response);
-
         let results = response.data;
 
         results.forEach(function(item){
-            //console.log(item);
 
             //Create variables
             let stillUrl = item.images.fixed_height_still.url;
             let movingUrl = item.images.fixed_height.url;
             let rating = item.rating;
-            //console.log("image: " + gif.stillUrl );
 
             //Create a div to display the gif in 
             let gifDiv = 
@@ -62,41 +58,64 @@ function displayGifs(search){
                 <p>Rating: ${rating}</p>
             </div>`;
 
-            $("#images-display").append(gifDiv);
+            //Append the div to the dom
+            $("#images-display").prepend(gifDiv);
 
         });
 
+        //Create new html to hold the "Show More" functionality 
+        let showMore = 
+        `<i class="fas fa-plus-square" ></i>
+        <div class="more">Show more GIFs</div>`;
+
+        //Add it to the page
+        $('#show-more').append(showMore);
     });
 
 }
 
-// Function: Create button --> add to array and call the display buttons function
+//Click handler to show more gifs on the page 
+$(document).on('click', '.show-more', function(){
+
+
+    localOffset++;
+
+    displayGifs(currentSearch, localOffset);
+
+});
+
+
+// Click handler for the submit button to grab the contents of the input field, add to the list of cities, and display new buttons
 $("input[type='submit']").on("click", function(event){
 
-    //
+    //Prevent for submitting early 
     event.preventDefault();
 
+    //Grab the input from the input field 
     let input = $("input[type='text']").val();
-    $("input[type='text']").val("");
+    $("input[type='text']").val(""); //Empty out the value after you submit
 
+    //If there is a value entered, add to the list and display the buttons
     if(input){
         citiesList.push(input);
         displayButtons();
     }
 
-    //console.log(input);
-
 });
 
-// Click handler: click on button --> pull from API and display all the giphs
+//Click handler for clicking on a specific city button to display the gifs
 $(document).on("click", ".button", function(){
     let search = $(this).attr("data-name");
-    console.log("Button name: " + search);
-    displayGifs(search);
+
+    //Assign to global variables to use in other functions
+    currentSearch = search;
+    localOffset = 0;
+
+    displayGifs(search, localOffset);
 });
 
 
-// Click handler: click on giph --> play it or pause it (add a data flag of some kind..?)
+//Click handler for the gif to make it move or not 
 $(document).on("click", "#gif-image", function(){
 
     //Check to see if the image is currently moving from a data flag set
@@ -114,7 +133,6 @@ $(document).on("click", "#gif-image", function(){
 
    
 });
-
 
 
 //Start the game 
